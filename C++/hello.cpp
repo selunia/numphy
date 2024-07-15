b8ec84e11a68b8088feec2437ddacee1ecea40d7
selunia <alena.b1998@yandex.ru>
2024-07-14 22:01:56 -0600

    Hello world C++ code added

---
+++ C++/hello.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+
+//one line comment
+/*
+Multiline block comment
+Another line
+oh! another one!
+*/
+
+std::cout << "Hello World from C++" << std::endl;
+
+return 0;
+}
