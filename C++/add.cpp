67dc3e4dbefbec2bc157d977a63d64e7dc24330b
selunia <alena.b1998@yandex.ru>
2024-07-14 22:23:54 -0600

    add.exe in C++

---
+++ C++/add.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+int main(int argc, char **argv)
+{
+int firstNumber = 7;
+int secondNumber = 10;
+int sum = firstNumber + secondNumber;
+std::cout << " The sum of the two numbers is: " << sum << std::endl;
+
+return 0;
+}
